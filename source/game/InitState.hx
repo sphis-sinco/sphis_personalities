e59ac4f3ec9d079fcc396e1237cf8a166ba9d84a
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-04 17:02:12 -0500

    load all scripts

--- source/game/InitState.hx
+++ source/game/InitState.hx
@@ -2,12 +2,15 @@ package game;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import game.scripts.ScriptManager;
 
 class InitState extends FlxState
 {
 
         override function create() {
                 super.create();
+
+		ScriptManager.loadAllScripts();
 
                 FlxG.switchState(() -> new game.desktop.DesktopMain());
         }
