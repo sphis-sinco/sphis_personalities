eec7a614470975b4373c9011a676270448077129
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-04 17:11:56 -0500

    instance

--- source/game/desktop/DesktopMain.hx
+++ source/game/desktop/DesktopMain.hx
@@ -1,11 +1,23 @@
 package game.desktop;
 
+import flixel.FlxSprite;
+
 class DesktopMain extends State
 {
+	public static var instance:DesktopMain = null;
+
 	override public function new()
 	{
 		super('desktop-main');
+
+		if (instance != null)
+		{
+			instance = null;
+		}
+		instance = this;
 	}
+
+	public var haxen:FlxSprite;
 
 	override public function create()
 	{
