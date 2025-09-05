79bfbc95eeb55942a72cdb5fdcf015fb1b9428ff
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-04 21:32:26 -0500

    only the play option will be enabled currently

---
+++ game/desktop/options/DesktopMainOptions.hx
@@ -0,0 +1,28 @@
+import flixel.FlxG;
+import flixel.util.FlxTimer;
+import game.desktop.DesktopMain;
+import game.scripts.events.CreateEvent;
+import game.scripts.events.UpdateEvent;
+
+var desktopMain:DesktopMain = null;
+
+function onCreate(event:CreateEvent)
+{
+	desktopMain = null;
+
+	if (event.state == 'desktop-main')
+		desktopMain = DesktopMain.instance;
+}
+
+function onUpdate(event:UpdateEvent)
+{
+	if (desktopMain != null && event.state == 'desktop-main')
+	{
+		desktopMain.option_play.alpha = 0.5;
+		if (FlxG.mouse.overlaps(desktopMain.option_play))
+		{
+			desktopMain.option_play.alpha = 1;
+		}
+		desktopMain.option_options.alpha = 0.5;
+	}
+}
