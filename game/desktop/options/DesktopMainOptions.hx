61c1704293a9e6d1614aa0205db11243a3901c9e
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-05 16:09:20 -0500

    haxen accept state

--- game/desktop/options/DesktopMainOptions.hx
+++ game/desktop/options/DesktopMainOptions.hx
@@ -1,12 +1,17 @@
+import flixel.FlxG;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import game.desktop.DesktopMain;
 import game.scripts.events.CreateEvent;
 import game.scripts.events.UpdateEvent;
 
 var desktopMain:DesktopMain = null;
+var moving:Bool = false;
 
 function onCreate(event:CreateEvent)
 {
 	desktopMain = null;
+	moving = false;
 
 	if (event.state == 'desktop-main')
 		desktopMain = DesktopMain.instance;
@@ -22,6 +27,31 @@ function onUpdate(event:UpdateEvent)
 		if (Mouse.overlaps(desktopMain.option_play))
 		{
 			desktopMain.option_play.alpha = 1;
+
+			if (Mouse.justReleased && !moving)
+			{
+				desktopMain.haxen_changeState('accept');
+				moving = true;
+				desktopMain.haxen.x -= (desktopMain.haxen.width / 10);
+				desktopMain.haxen.y -= (desktopMain.haxen.height / 20);
+
+				FlxTween.tween(desktopMain.haxen, {y: FlxG.height + desktopMain.haxen.height}, 1, {
+					ease: FlxEase.sineInOut,
+					startDelay: 0.5,
+					onComplete: tween ->
+					{
+						FlxG.switchState(() -> new DesktopMain());
+					}
+				});
+
+				FlxTween.tween(desktopMain.option_play, {y: FlxG.height + desktopMain.option_play.height}, 1, {
+					ease: FlxEase.sineInOut,
+					startDelay: 0.1,
+				});
+				FlxTween.tween(desktopMain.option_options, {y: FlxG.height + desktopMain.option_options.height}, 1, {
+					ease: FlxEase.sineInOut
+				});
+			}
 		}
 		else if (Mouse.overlaps(desktopMain.option_options))
 		{
