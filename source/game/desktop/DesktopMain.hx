0f08b17623b346f742b9c05acd964a50b4de506e
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-04 20:53:17 -0500

    missing {} now

--- source/game/desktop/DesktopMain.hx
+++ source/game/desktop/DesktopMain.hx
@@ -31,9 +31,7 @@ class DesktopMain extends State
 	}
 
 	public function haxen_idle()
-	{
 		haxen_changeState('idle-' + haxen_random_dir());
-	}
 
 	public function haxen_random_dir()
 	{
@@ -41,9 +39,7 @@ class DesktopMain extends State
 	}
 
 	public function haxen_changeState(newState:String)
-	{
 		haxen.loadGraphic(Paths.getImagePath('desktop/haxen/' + newState));
-	}
 
 	override public function update(elapsed:Float)
 	{
