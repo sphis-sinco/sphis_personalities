b03cb380041f1c353aa92d5bbf0bf11887192d59
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-05 15:49:59 -0500

    blank mouse state

--- source/game/MouseStates.hx
+++ source/game/MouseStates.hx
@@ -2,6 +2,7 @@ package game;
 
 class MouseStates
 {
+	public static var BLANK = 'blank';
 	public static var IDLE = 'idle';
 	public static var CAN_SELECT = 'can-select';
 	public static var CANT_SELECT = 'cant-select';
