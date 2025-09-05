4810754b0632bb57a77586e17e86d9bf105bc37c
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-04 22:25:10 -0500

    whatever

--- source/game/Mouse.hx
+++ source/game/Mouse.hx
@@ -6,23 +6,17 @@ import openfl.display.Bitmap;
 
 class Mouse
 {
-	public static var previous_state:String = '';
 	public static var state:String = MouseStates.IDLE;
 
 	public static function setMouseState(value:String)
 	{
-		previous_state = '' + state;
 		state = value;
 		updateMouseGraphic();
 	}
 
 	public static function updateMouseGraphic()
 	{
-		if (previous_state == state)
-			return;
-
 		var graphic = FlxGraphic.fromAssetKey(Paths.getImagePath('mouse/' + state));
-		trace('Updating Mouse Graphic to: ' + graphic.key);
 
 		if (graphic == null)
 		{
