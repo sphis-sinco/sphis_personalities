72bc6ae43d90579eec9a67f11e6bdf30d2852afb
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-05 21:36:23 -0500

    gridlike scanlines + bigger scanlines

--- game/LCD/LCDDisplay.hx
+++ game/LCD/LCDDisplay.hx
@@ -5,32 +5,45 @@ import game.desktop.DesktopPlay;
 import game.scripts.events.CreateEvent;
 import game.scripts.events.UpdateEvent;
 
-var scanlines:FlxSprite;
-var acceptedStates = ['desktop-main', 'desktop-play'];
+var scanlineLayerOne:FlxSprite;
+var scanlineLayerTwo:FlxSprite;
+var scanlineAcceptedStates = ['desktop-main', 'desktop-play'];
+var scanlineAngle = FlxG.random.float(0, 360);
 
 function onCreate(event:CreateEvent)
 {
-	scanlines = new FlxSprite();
-	scanlines.loadGraphic(Paths.getImagePath('LCD/scanlines'));
-	scanlines.screenCenter();
-	scanlines.scrollFactor.set(0, 0);
+	scanlineLayerOne = new FlxSprite();
+	scanlineLayerOne.loadGraphic(Paths.getImagePath('LCD/scanlines'));
+	scanlineLayerOne.screenCenter();
+	scanlineLayerOne.scrollFactor.set(0, 0);
 
-	if (acceptedStates.contains(event.state))
+	scanlineLayerTwo = new FlxSprite();
+	scanlineLayerTwo.loadGraphic(Paths.getImagePath('LCD/scanlines'));
+	scanlineLayerTwo.screenCenter();
+	scanlineLayerTwo.scrollFactor.set(0, 0);
+
+	if (scanlineAcceptedStates.contains(event.state))
 	{
 		switch (event.state)
 		{
 			case 'desktop-main':
-				DesktopMain.instance.scanlineLayer.add(scanlines);
+				DesktopMain.instance.scanlineLayer.add(scanlineLayerOne);
+				DesktopMain.instance.scanlineLayer.add(scanlineLayerTwo);
 			case 'desktop-play':
-				DesktopPlay.instance.scanlineLayer.add(scanlines);
+				DesktopPlay.instance.scanlineLayer.add(scanlineLayerOne);
+				DesktopPlay.instance.scanlineLayer.add(scanlineLayerTwo);
 		}
 	}
 }
 
 function onUpdate(event:UpdateEvent)
 {
-	if (scanlines != null)
+	if (scanlineLayerOne != null)
 	{
-		scanlines.alpha = FlxG.random.float(0.15, 0.2);
+		scanlineAngle += (1 / FlxG.random.int(10, 100));
+		scanlineLayerOne.angle = scanlineAngle;
+		scanlineLayerOne.alpha = FlxG.random.float(0, 0.2);
+		scanlineLayerTwo.angle = scanlineAngle + 90;
+		scanlineLayerTwo.alpha = scanlineLayerOne.alpha;
 	}
 }
