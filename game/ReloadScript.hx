fdb25e8b5e810a6c951811a545839b5a1a067b3d
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-06 03:59:18 -0500

    loadAllScripts

--- game/ReloadScript.hx
+++ game/ReloadScript.hx
@@ -1,11 +1,14 @@
 import flixel.FlxG;
 import game.State;
+import game.scripts.ScriptManager;
 import game.scripts.events.UpdateEvent;
 
 function onUpdate(event:UpdateEvent)
 {
 	if (FlxG.keys.anyJustReleased([State.reloadKey]))
 	{
+		ScriptManager.loadAllScripts();
+
 		FlxG.resetState();
 	}
 }
