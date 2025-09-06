0cd5a934b769402e3f50c1c6a81c27700bfc98b8
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-05 22:46:06 -0500

    transitions

--- game/desktop/easterEgg/EasterEggTransition.hx
+++ game/desktop/easterEgg/EasterEggTransition.hx
@@ -1,11 +1,46 @@
 import flixel.FlxG;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
 import game.scripts.events.UpdateEvent;
+
+var transitioning = false;
 
 function onUpdate(event:UpdateEvent)
 {
-	if (FlxG.keys.justReleased.F1 || (FlxG.keys.pressed.SHIFT && FlxG.keys.justReleased.ONE))
+	if (!transitioning && (FlxG.keys.justReleased.F1 || (FlxG.keys.pressed.SHIFT && FlxG.keys.justReleased.ONE)))
 	{
+		transitioning = true;
 		if (event.state != 'z-easter-egg')
-			FlxG.switchState(() -> new BlankState('z-easter-egg'));
+		{
+			if (event.state == 'desktop-main')
+			{
+				var state = DesktopMain.instance;
+
+				FlxTween.tween(state.haxen, {alpha: 0}, 1, {
+					onComplete: _ ->
+					{
+						FlxG.switchState(() -> new BlankState('z-easter-egg'));
+					}
+				});
+				FlxTween.tween(state.option_play, {alpha: 0});
+				FlxTween.tween(state.option_options, {alpha: 0});
+			}
+			else if (event.state == 'desktop-play')
+			{
+				var state = DesktopPlay.instance;
+
+				for (obj in state.levelsGrp)
+					FlxTween.tween(obj, {alpha: 0});
+				for (obj in state.levelsTextGrp)
+					FlxTween.tween(obj, {alpha: 0});
+
+				FlxTimer.wait(1, () ->
+				{
+					FlxG.switchState(() -> new BlankState('z-easter-egg'));
+				});
+			}
+			else
+				FlxG.switchState(() -> new BlankState('z-easter-egg'));
+		}
 	}
 }
