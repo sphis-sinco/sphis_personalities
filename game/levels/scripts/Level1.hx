493c946ec5840f9a869137dc2036b10e38bbe152
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-07 16:05:43 -0500

    lvl 1 pausing and leaving

--- game/levels/scripts/Level1.hx
+++ game/levels/scripts/Level1.hx
@@ -1,4 +1,5 @@
 import flixel.FlxG;
+import game.desktop.DesktopPlay;
 import game.desktop.play.LevelModule;
 import game.scripts.events.CreateEvent;
 import game.scripts.events.UpdateEvent;
@@ -6,10 +7,12 @@ import game.scripts.imports.FlxScriptedColor;
 
 var level_one:LevelModule;
 var is_level_one:Bool;
+var level_paused:Bool;
 
 function onCreate(event:CreateEvent)
 {
 	is_level_one = (event.state == 'level1');
+	level_paused = false;
 	if (is_level_one)
 	{
 		level_one = new LevelModule(event.state);
@@ -17,4 +20,20 @@ function onCreate(event:CreateEvent)
 	}
 }
 
-function onUpdate(event:UpdateEvent) {}
+function onUpdate(event:UpdateEvent)
+{
+	if (is_level_one)
+	{
+		if (Controls.getControlJustReleased('game_pause'))
+		{
+			level_paused = !level_paused;
+		}
+		if (Controls.getControlJustReleased('ui_leave') && level_paused)
+		{
+			FlxG.camera.fade(FlxScriptedColor.BLACK, 1, false, () ->
+			{
+				FlxG.switchState(() -> new DesktopPlay());
+			});
+		}
+	}
+}
