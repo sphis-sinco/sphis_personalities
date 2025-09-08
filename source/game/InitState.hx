089480a66928cc882b417c516720373b93ca8c96
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-07 20:42:32 -0500

    console registered classNames

--- source/game/InitState.hx
+++ source/game/InitState.hx
@@ -6,8 +6,22 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import game.Controls.ControlsSave;
 import game.desktop.DesktopMain;
+import game.desktop.DesktopPlay;
+import game.desktop.play.LevelModule;
+import game.desktop.play.LevelSpriteGroup;
 import game.scripts.ScriptManager;
+import game.scripts.WebScripts;
+import game.scripts.events.AddedEvent;
+import game.scripts.events.BaseEvent;
+import game.scripts.events.BaseStateEvent;
+import game.scripts.events.CreateEvent;
+import game.scripts.events.UpdateEvent;
+import game.scripts.imports.FlxScriptedAxes;
+import game.scripts.imports.FlxScriptedColor;
 import haxe.macro.Compiler;
+#if hscript
+import flixel.system.debug.console.ConsoleUtil;
+#end
 
 class InitState extends FlxState
 {
@@ -38,6 +52,36 @@ class InitState extends FlxState
 		#if !html5
 		haxe.Log.trace = Iris.print;
 		#end
+		#end
+
+		#if hscript
+		ConsoleUtil.registerObject('InitState', InitState);
+		ConsoleUtil.registerObject('Controls', Controls);
+		ConsoleUtil.registerObject('Mouse', Mouse);
+		ConsoleUtil.registerObject('MouseStates', MouseStates);
+		ConsoleUtil.registerObject('Paths', Paths);
+		ConsoleUtil.registerObject('State', State);
+		ConsoleUtil.registerObject('Ansi', Ansi);
+		ConsoleUtil.registerObject('BlankState', BlankState);
+
+		ConsoleUtil.registerObject('DesktopMain', DesktopMain);
+		ConsoleUtil.registerObject('DesktopPlay', DesktopPlay);
+
+		// ConsoleUtil.registerObject('LevelData', LevelData);
+		ConsoleUtil.registerObject('LevelModule', LevelModule);
+		ConsoleUtil.registerObject('LevelSpriteGroup', LevelSpriteGroup);
+
+		ConsoleUtil.registerObject('ScriptManager', ScriptManager);
+		ConsoleUtil.registerObject('WebScripts', WebScripts);
+
+		ConsoleUtil.registerObject('AddedEvent', AddedEvent);
+		ConsoleUtil.registerObject('BaseEvent', BaseEvent);
+		ConsoleUtil.registerObject('BaseStateEvent', BaseStateEvent);
+		ConsoleUtil.registerObject('CreateEvent', CreateEvent);
+		ConsoleUtil.registerObject('UpdateEvent', UpdateEvent);
+
+		ConsoleUtil.registerObject('FlxScriptedAxes', FlxScriptedAxes);
+		ConsoleUtil.registerObject('FlxScriptedColor', FlxScriptedColor);
 		#end
 
 		#if generateWebScript
