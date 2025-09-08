7433c6b7f50f450609b72c65b6bdf086817d8e2b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-08 09:35:34 -0500

    level xmls are read now :DDDDDDDDD + loadedModules so level modules arent being reloaded and reloaded all the time

--- source/game/desktop/play/LevelModule.hx
+++ source/game/desktop/play/LevelModule.hx
@@ -7,6 +7,8 @@ import haxe.xml.Access;
 
 class LevelModule
 {
+	public static var loadedModules:Map<String, LevelModule> = [];
+
 	public var displayName:String;
 	public var authors:Array<String>;
 	public var unlocked:Bool;
@@ -15,26 +17,134 @@ class LevelModule
 
 	public var assetFolders:AssetFolders;
 
-	public function new(dataFileid:String)
+	public function new(levelID:String)
 	{
-		final JSONPath = Paths.getGamePath('levels/data/' + dataFileid + '.json');
-		final XMLPath = Paths.getGamePath('levels/data/' + dataFileid + '.xml');
+		if (!loadedModules.exists(levelID))
+		{
+			loadModule(levelID);
+		}
+		else
+		{
+			trace('Level(' + levelID + ') Module already loaded');
+			var module = loadedModules.get(levelID);
+
+			try
+			{
+				this.assetFolders = module.assetFolders;
+				this.authors = module.authors;
+				this.displayName = module.displayName;
+				this.id = module.id;
+				this.unlocked = module.unlocked;
+			}
+			catch (_)
+			{
+				loadModule(levelID);
+			}
+		}
+	}
+
+	public function loadModule(levelID:String)
+	{
+		trace('Initalizing new Level(' + levelID + ') Module');
+
+		final JSONPath = Paths.getGamePath('levels/data/' + levelID + '.json');
+		final XMLPath = Paths.getGamePath('levels/data/' + levelID + '.xml');
+
+		final jsonData:LevelData = Json.parse(Paths.getText(JSONPath));
+		final xmlData = new Access(Xml.parse(Paths.getText(XMLPath)).firstElement());
+
+		assetFolders = {
+			haxen: 'general',
+			hand: 'general',
+			general: 'one'
+		};
 
 		try
 		{
-			final jsonData:LevelData = Json.parse(Paths.getText(JSONPath));
-			final xmlData = new Access(Xml.parse(Paths.getText(XMLPath)));
-			trace(xmlData);
-
-			displayName = jsonData.displayName;
-			authors = jsonData.authors;
-			unlocked = jsonData.unlocked;
-			id = (jsonData.id ?? dataFileid);
-			assetFolders = jsonData.assetFolders;
+			parseXML(xmlData, levelID);
 		}
 		catch (e)
 		{
-			#if !html5 Iris.error #else trace #end ('Cannot get level data file: ' + dataFileid + '\n\n' + e.message);
+			#if !html5 Iris.warn #else trace #end ('Cannot get level data file (XML): ' + levelID + '\n\n' + e.message);
+
+			try
+			{
+				parseJSON(jsonData, levelID);
+			}
+			catch (e)
+			{
+				#if !html5 Iris.error #else trace #end ('Cannot get level data file (JSON): ' + levelID + '\n\n' + e.message);
+			}
+		}
+
+		loadedModules.set(levelID, this);
+	}
+
+	public function parseJSON(data:LevelData, levelID:String)
+	{
+		trace('Level(' + levelID + ') JSON being parsed');
+		displayName = data.displayName;
+		authors = data.authors;
+		unlocked = data.unlocked;
+		id = (data.id ?? levelID);
+		assetFolders = data.assetFolders;
+	}
+
+	public function parseXML(data:Access, levelID:String)
+	{
+		for (element in data.elements)
+		{
+			try
+			{
+				trace('Level(' + levelID + ') XML Element(' + element.name + ') being parsed');
+
+				if (element.name.toLowerCase() == 'displayname')
+				{
+					displayName = element.att.value;
+				}
+				if (element.name.toLowerCase() == 'authors')
+				{
+					authors = [];
+					for (author in element.elements)
+					{
+						authors.push(author.att.value);
+					}
+				}
+				if (element.name.toLowerCase() == 'unlocked')
+				{
+					unlocked = (element.att.value.toLowerCase() == 'true' || element.att.value.toLowerCase() == '1');
+				}
+				if (element.name.toLowerCase() == 'assetfolders')
+				{
+					try
+					{
+						assetFolders.haxen = 'general';
+						assetFolders.hand = 'general';
+						assetFolders.general = 'one';
+
+						for (assetFolder in element.elements)
+						{
+							switch (assetFolder.name)
+							{
+								case 'haxen':
+									assetFolders.haxen = assetFolder.att.value;
+								case 'hand':
+									assetFolders.hand = assetFolder.att.value;
+								case 'general':
+									assetFolders.general = assetFolder.att.value;
+							}
+						}
+					}
+					catch (e)
+					{
+						trace('assetFolders XML failed: ' + e.message);
+					}
+				}
+			}
+			catch (e)
+			{
+				trace(element.name + ' did smth: ' + e.message);
+			}
 		}
 	}
 
