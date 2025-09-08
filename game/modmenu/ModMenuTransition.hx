00910d2e6d48ded72272586a2a2e76bcb649dd8a
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-08 15:25:37 -0500

    instant transition

--- game/modmenu/ModMenuTransition.hx
+++ game/modmenu/ModMenuTransition.hx
@@ -1,35 +1,10 @@
 import flixel.FlxG;
-import flixel.tweens.FlxTween;
-import flixel.util.FlxTimer;
 import game.modding.ModMenu;
-import game.scripts.ScriptManager;
 import game.scripts.events.UpdateEvent;
-import game.scripts.imports.FlxScriptedColor;
-
-var transitioning = false;
-
-function onCreate(event:UpdateEvent)
-{
-	transitioning = false;
-
-	if (event.state == 'modmenu')
-	{
-		FlxG.camera.fade(FlxScriptedColor.BLACK, 1, true, null);
-	}
-}
 
 function onUpdate(event:UpdateEvent)
 {
-	if (!transitioning && (FlxG.keys.justReleased.F4 || (FlxG.keys.pressed.SHIFT && FlxG.keys.justReleased.FOUR)))
-	{
+	if (FlxG.keys.justReleased.F4 || (FlxG.keys.pressed.SHIFT && FlxG.keys.justReleased.FOUR))
 		if (event.state == 'desktop-main' || event.state == 'desktop-play')
-		{
-			transitioning = true;
-
-			FlxG.camera.fade(FlxScriptedColor.BLACK, 1, false, () ->
-			{
-				FlxG.switchState(() -> new ModMenu());
-			});
-		}
-	}
+			FlxG.switchState(() -> new ModMenu());
 }
