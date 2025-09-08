13d170ec23f661182a59a7f7903d68c423ea530f
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-08 17:14:50 -0500

    i dont feel the need to explain myself

---
+++ source/game/scripts/WebScripts-Base.hx
@@ -0,0 +1,14 @@
+package game.scripts;
+
+import game.scripts.events.AddedEvent;
+import game.scripts.events.CreateEvent;
+import game.scripts.events.UpdateEvent;
+
+class WebScripts
+{
+	public static function onAdded(event:AddedEvent) {}
+
+	public static function onCreate(event:CreateEvent) {}
+
+	public static function onUpdate(event:UpdateEvent) {}
+}
