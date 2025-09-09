4061cbf527e6048f4ef899bc0677b613938c96fa
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-08 20:10:14 -0500

    its not working.

--- game/levels/DesktopPlayOpening.hx
+++ game/levels/DesktopPlayOpening.hx
@@ -18,9 +18,6 @@ function onCreate(event:CreateEvent)
 				FlxTween.tween(obj.levelIcon, {alpha: obj.targAlpha}, 1, {
 					ease: FlxEase.sineInOut
 				});
-				if (FlxG.save != null && FlxG.save.data.newlevels != null)
-					if (FlxG.save.data.newlevels.contains(obj.levelID))
-						obj.lock.visible = true;
 				obj.lock.alpha = 0;
 				FlxTween.tween(obj.lock, {alpha: 1}, 1, {
 					ease: FlxEase.sineInOut
@@ -32,29 +29,6 @@ function onCreate(event:CreateEvent)
 				FlxTween.tween(obj, {alpha: 1}, 1, {
 					ease: FlxEase.sineInOut
 				});
-			}
-
-			if (FlxG.save != null && FlxG.save.data.newlevels != null)
-			{
-				var newlevels:Array<String> = FlxG.save.data.newlevels;
-
-				for (level in newlevels)
-				{
-					var levelGrp = DesktopPlay.instance.levelsGrp.getFirst(function(levelGrp:LevelSpriteGroup)
-					{
-						return levelGrp.levelID == level;
-					});
-
-					FlxTimer.wait(1 * (levelGrp.ID + 1), () ->
-					{
-						DesktopPlay.instance.camFollow.x = levelGrp.levelIcon.x;
-						levelGrp.lock.loadGraphic(Paths.getImagePath('levels/desktop-icons/unlock'));
-						FlxTimer.wait(.25, () ->
-						{
-							FlxFlicker.flicker(levelGrp.lock, 1, 0.04, false);
-						});
-					});
-				}
 			}
 		});
 	}
