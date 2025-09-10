448b5d1c32b23d5379451ff5b479c47adfb4f519
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-10 16:10:48 -0500

    level 2 isn't in v1

--- game/levels/scripts/Level2.hx
+++
@@ -1,7 +0,0 @@
-import game.scripts.events.AddedEvent;
-import game.scripts.events.CreateEvent;
-import game.scripts.events.UpdateEvent;
-
-function onAdded(event:AddedEvent) {}
-function onCreate(event:CreateEvent) {}
-function onUpdate(event:UpdateEvent) {}
